13783811f33d22c5440d955f7dd9c56d3006a29d
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-03-06 21:26:21 -0700

    Fixed structure

---
+++ my_torch/nn.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Linear:
+	def __init__(self, no_input_neurons, no_output_neurons):
+		self.no_input_neurons = no_input_neurons
+		self.no_output_neurons = no_output_neurons
+		# _linear()
+
+	def _linear(self):
+		print(f"Input: {self.no_input_neurons}, Output: {self.no_output_neurons}")
+		# return (self.no_input_neurons, self.no_output_neurons)
+		
+
+if __name__ == "__main__":
+	pass
--- running.py
+++ running.py
@@ -1,1 +1,5 @@
 # Inital file 
+from my_torch import nn
+
+obj = nn.Linear(5, 10)
+obj._linear()
